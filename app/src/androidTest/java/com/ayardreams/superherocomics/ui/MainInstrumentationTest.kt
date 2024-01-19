02179ee3873c398c90b05307197a10324e9708d8
AdrianRaya <adrian.raya-hernandez@domakaba-external.com>
2024-01-19 14:04:44 +0100

    Test Instrumentation

--- app/src/androidTest/java/com/ayardreams/superherocomics/ui/MainInstrumentationTest.kt
+++ app/src/androidTest/java/com/ayardreams/superherocomics/ui/MainInstrumentationTest.kt
@@ -1,7 +1,16 @@
 package com.ayardreams.superherocomics.ui
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.ayardreams.superherocomics.R
 import com.ayardreams.superherocomics.data.server.MockWebServerRule
 import com.ayardreams.superherocomics.data.server.fromJson
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -11,6 +20,7 @@ import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockResponse
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -24,6 +34,11 @@ class MainInstrumentationTest {
     val mockWebServerRule = MockWebServerRule()
 
     @get:Rule(order = 2)
+    val locationPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        "android.permission.CAMERA"
+    )
+
+    @get:Rule(order = 3)
     val activityRule = ActivityScenarioRule(NavHostActivity::class.java)
 
     @Inject
@@ -39,5 +54,42 @@ class MainInstrumentationTest {
 
         val resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient)
         IdlingRegistry.getInstance().register(resource)
+    }
+
+    @Test
+    fun click_a_comic_navigates_to_detail() {
+        Espresso.onView(withId(R.id.btnNewComics)).perform(ViewActions.click())
+
+        Espresso.onView(withId(R.id.recycler))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    ViewActions.click()
+                )
+            )
+
+        Espresso.onView(withId(R.id.resume_comics_summary))
+            .check(
+                ViewAssertions.matches(
+                    ViewMatchers.withText(
+                        "The Avengers battle the Twilight Court...for " +
+                            "the sake of Kang the Conqueror? The Twilight Court wishes to bring " +
+                            "Kang to justice, but the Avengers still have need of the comatose " +
+                            "conqueror. Which side can claim to truly be just?"
+                    )
+                )
+            )
+    }
+
+    @Test
+    fun click_a_comic_navigates_to_reader_qr() {
+        Espresso.onView(withId(R.id.btnReaderComics)).perform(ViewActions.click())
+
+        Espresso.onView(withId(R.id.btnStartReader)).perform(ViewActions.click())
+
+        Espresso.pressBack()
+
+        Espresso.onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Lector QR Cancelado")))
     }
 }
